28636d7558e157b8d5ee782185947ae7f20e7b9a
kronos0409 <jaranicolas101@gmail.com>
2025-02-20 19:39:00 -0300

    Delete chat_ia/navegacion.py

--- chat_ia/navegacion.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.sidebar.title("Navegación")
-pagina = st.sidebar.radio("Ir a:", ["Inicio", "IA"])
-
-if pagina == "IA":
-    st.switch_page("pages/IA")
-else:
-    st.title("Bienvenido a la app principal")
